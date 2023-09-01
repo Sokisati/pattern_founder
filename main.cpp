f9235cc2afb6cff4798711c0f504c6aa21bf5a67
Sokisati <yagizalpersoy@gmail.com>
2023-09-02 00:03:05 +0300

    added the tempI variable (small subpatterns inside a pattern isn't a problem anymore thanks to this variable)

--- main.cpp
+++ main.cpp
@@ -9,53 +9,52 @@
 using namespace std;
 
 
-
 vector<int> patternFinderFunction(vector<int> aliveCellsVector)
 {
 
-    int i,c,p,checkAgain,t;
+    int i,c,p,checkAgain,t,tempI;
     bool searchBool = false;
+    vector<int> patternFound;
 
     i = 1;
     p = 0;
-    checkAgain = 1;
-
-
-    vector<int> patternFound;
-
+    tempI = i;
 
     for(i; i<aliveCellsVector.size(); i++)
     {
         searchBool = false;
         checkAgain = 1;
+        tempI = i;
 
-
-
-        for(c=i-1; c>1; c--)
+        for(c=i-1; c>=1; c--)
         {
             if(searchBool)
             {
+                i = tempI;
                 break;
             }
 
             while(aliveCellsVector[c]==aliveCellsVector[i])
             {
-
-                searchBool = true;
                 p++;
-                if(i-c-p==0)
+                searchBool = true;
+
+                if(i-c-p<=0)
                 {
                     if(checkAgain<2)
                     {
                         checkAgain++;
                         t = c;
                         c = i;
+                        tempI = i;
                         i = 2*i - t;
                         p = 0;
                     }
-                    else{
+                    else
+                    {
                         p = 0;
-                        for (p; i - c - p > 0; p++) {
+                        for (p; i - c - p > 0; p++)
+                        {
                             patternFound.push_back(aliveCellsVector[i + p]);
                         }
                         return patternFound;
@@ -66,6 +65,7 @@ vector<int> patternFinderFunction(vector<int> aliveCellsVector)
                 if(aliveCellsVector[c+p]!=aliveCellsVector[i+p] || i-c>8)
                 {
                     p = 0;
+                    i = tempI;
                     break;
                 }
             }
@@ -73,45 +73,4 @@ vector<int> patternFinderFunction(vector<int> aliveCellsVector)
     }
 
     return patternFound;
-}
-
-int main() {
-    srand(time(0));
-
-
-    vector<int> aliveCellsVector;
-    vector<int> foundPattern;
-
-    int randomNumber;
-
-
-
-    while(true){
-
-        randomNumber = (rand() % 8)+2;
-        aliveCellsVector.push_back(randomNumber);
-        cout<<randomNumber<<" ";
-
-
-
-        foundPattern=patternFinderFunction(aliveCellsVector);
-
-        if(!foundPattern.empty()){
-            for (int i = 0; i < foundPattern.size(); i++) {
-
-                cout<<endl<<"-------"<<endl;
-                cout<<foundPattern[i]<<" ";
-            }
-            return 0;
-        }
-
-    }
-
-
-
-
-
-
-
-    return 0;
 }
